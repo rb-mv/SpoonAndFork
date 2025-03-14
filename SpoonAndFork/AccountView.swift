adfd6c761d291d184ecdf3f58257070d99226856
rb-mv <radzhab._.magomedov@icloud.com>
2025-03-14 18:14:13 +0300

    Added navigation and couple of views

---
+++ SpoonAndFork/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountVeiw.swift
+//  SpoonAndFork
+//
+//  Created by Раджаб Магомедов on 14.03.2025.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text("Account view!")
+    }
+}
+
+#Preview {
+    AccountView()
+}
---
+++ SpoonAndFork/SpoonAndForkListView.swift
@@ -0,0 +1,18 @@
+//
+//  SpoonAndForkListView.swift
+//  SpoonAndFork
+//
+//  Created by Раджаб Магомедов on 14.03.2025.
+//
+
+import SwiftUI
+
+struct SpoonAndForkListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpoonAndForkListView()
+}
