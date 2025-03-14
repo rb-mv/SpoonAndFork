878ab3d9c0b20ce1c0de6376de2754c13a1d8943
rb-mv <radzhab._.magomedov@icloud.com>
2025-03-14 17:44:59 +0300

    Initial Commit

---
+++ SpoonAndFork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpoonAndFork
+//
+//  Created by Раджаб Магомедов on 14.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpoonAndFork/SpoonAndForkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoonAndForkApp.swift
+//  SpoonAndFork
+//
+//  Created by Раджаб Магомедов on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpoonAndForkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
